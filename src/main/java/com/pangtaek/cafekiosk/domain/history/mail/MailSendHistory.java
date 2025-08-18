470cd0c5f0f579ddbee3e32b0ad60d9e232c9e90
Pangtaek <zombie3632@gmail.com>
2025-08-18 19:26:58 +0900

    test: OrderStatisticsService 테스트 구현

---
+++ src/main/java/com/pangtaek/cafekiosk/domain/history/mail/MailSendHistory.java
@@ -0,0 +1,31 @@
+package com.pangtaek.cafekiosk.domain.history.mail;
+
+import com.pangtaek.cafekiosk.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "mail_send_history")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class MailSendHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    private String fromEmail;
+    private String toEmail;
+    private String subject;
+    private String content;
+
+    @Builder
+    public MailSendHistory(String fromEmail, String toEmail, String subject, String content) {
+        this.fromEmail = fromEmail;
+        this.toEmail = toEmail;
+        this.subject = subject;
+        this.content = content;
+    }
+}
